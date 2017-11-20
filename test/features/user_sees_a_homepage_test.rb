99dcab8e2c23d0157a60bf3a3f0792f86e2f722f
heybluguy <heybluguy@yahoo.com>
2017-11-20 14:33:16 -0700

    capybara

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -3,9 +3,8 @@ require './test/test_helper'
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
-    save_and_open_page
+    # save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
-
   end
 end
